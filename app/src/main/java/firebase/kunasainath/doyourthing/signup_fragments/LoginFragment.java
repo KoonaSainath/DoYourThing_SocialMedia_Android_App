39bc4494e7e900235fa26cc588165e91c2bba775
KunaSainath <sainathwnp@gmail.com>
2020-12-17 19:10:09 +0530

    signup and login fragments designing is done

---
+++ app/src/main/java/firebase/kunasainath/doyourthing/signup_fragments/LoginFragment.java
@@ -0,0 +1,33 @@
+package firebase.kunasainath.doyourthing.signup_fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import firebase.kunasainath.doyourthing.R;
+public class LoginFragment extends Fragment {
+
+    public LoginFragment() {
+    }
+
+    public static LoginFragment newInstance() {
+        LoginFragment fragment = new LoginFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
