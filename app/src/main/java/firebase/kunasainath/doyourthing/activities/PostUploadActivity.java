892f714904acba4a45e81139fe095b5fe15da811
KunaSainath <sainathwnp@gmail.com>
2020-12-18 14:32:04 +0530

    created PostUploadActivity.java

---
+++ app/src/main/java/firebase/kunasainath/doyourthing/activities/PostUploadActivity.java
@@ -0,0 +1,14 @@
+package firebase.kunasainath.doyourthing.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostUploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_upload);
+    }
+}
